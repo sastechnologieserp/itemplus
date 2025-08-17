d6ce736c494343a8636403e738e7b1f76aba4665
yosef ali <joeyxjoey123@gmail.com>
2025-08-17 09:53:41 +0000

    added new doctype for pay pay out cash

---
+++ itemplus/itemplus/doctype/petty_cash/test_petty_cash.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, info@saserp.co and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestPettyCash(FrappeTestCase):
+	pass
